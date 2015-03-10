56cdce2275e056aa5f675c28b66f4beebe1f6319
Liam Hamill <hamilllr@gmail.com>
2015-03-10 23:35:37 +0000

    Added prototype group viewer GUI

---
+++ group_viewer.py
@@ -0,0 +1,56 @@
+from tkinter import *
+import students
+import groups
+
+class group_viewer(Frame):
+
+	def __init__(self, master, group_list):
+
+		Frame.__init__(self, master)
+		self.master = master
+
+		header = Label(self, text = "Group Viewer", font = ('MS', 20, 'bold'))
+		header.grid()
+
+		self.grid()
+		self.position = 0
+		
+		for group in group_list:
+			self.display_group(group)
+			self.position += 2
+
+	def display_group(self, group):
+
+		list_frame = Frame(self.master)
+		list_frame.grid(row = 1,column = self.position)
+
+		scrollbar = Scrollbar(list_frame) 
+		scrollbar.grid(row = 0, column = 1)
+
+		yscrollcommand=scrollbar.set
+		
+		listbox = Listbox(list_frame)
+		for student in group.contents:
+			listbox.insert(END, student.firstname + ' ' + student.surname)
+
+		listbox.grid(row = 0, column = 0)
+		
+		scrollbar.config(command=listbox.yview)
+
+	
+
+
+if __name__ == "__main__":
+	root = Tk()
+	root.title("Group Viewer")
+
+	student_list = [students.Student("Liam", "Hamill", 1, "email")] * 5
+	
+	test_group = groups.Group(1)
+	for item in student_list:
+		test_group.add_student(item)
+
+	window = group_viewer(root, [test_group]*50)
+
+
+	window.mainloop()
