e94a07f79a086da4de5a733055489c8c5397d4d5
Andrew Rowlands <ajr.hpc@gmail.com>
2015-03-16 11:53:02 +0000

    Updated Personality git push

---
+++ Personality.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import tkinter.messagebox
+from students import *
+
+class Personality (Frame):
+
+    def __init__ (self, master):
+
+        super(Personality, self).__init__(master)
+        self.master = master
+        self.grid()
+        
+        
