9bddb77c9f241c285f6c1defae3aba8965db6387
Liam Hamill <hamilllr@gmail.com>
2015-04-01 13:55:46 +0100

    Added competency attribute to students class

--- students.py
+++ students.py
@@ -1,6 +1,6 @@
 class Student ():
 
-    def __init__(self, firstname, surname, number, email, grades = None, role = None):
+    def __init__(self, firstname, surname, number, email, grades = None, role = None, competency = False):
         # Students must have a name, number, and email.
         # Optionally, you can also define grades and role at this point.
 
@@ -10,12 +10,13 @@ class Student ():
         self.email = email
         self.grades = grades
         self.role = role
+        self.competency = competency
 
     def __str__(self):
         # Returns a one-line string containing a Student's attributes.
         # May be useful for debugging.
 
-        output = "(Firstname: {}, Surname: {}, Number: {}, Email: {}".format(self.firstname, self.surname, self.number, self.email)
+        output = "(Firstname: {}, Surname: {}, Number: {}, Email: {}, Competency: {}".format(self.firstname, self.surname, self.number, self.email, self.competency)
         
         if self.grades:
             output += ", Grades: {}".format(self.grades)
@@ -31,6 +32,9 @@ class Student ():
     def addGrade (self, grade):
         self.grade = grade      
 
+    def changeCompetency (self, competency):
+        self.competency = competency
+
     def getStudentInfo (self):
         # Prints student attributes to the console over several lines.
         # This will have to be rewritten for the GUI.
@@ -38,6 +42,7 @@ class Student ():
         print("Name:", self.firstname, self.surname)
         print("Student Number:", self.number)
         print("Email:", self.email)
+        print("Competency:", self.competency)
 
         if self.grades:
             print("Grades:", self.grades)
@@ -51,7 +56,7 @@ if __name__ == "__main__":
     # Test/Debug code
     # Delete before submission
 
-    aStudent = Student("Liam", "Hammill", 1, "email")
+    aStudent = Student("Liam", "Hammill", 1, "email", competency = True)
     aStudent2 = Student("Test","Tester",  2, "email", 70, "role")
 
     aStudent.getStudentInfo()
